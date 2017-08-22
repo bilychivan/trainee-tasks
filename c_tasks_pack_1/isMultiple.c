b1006907ea20a5319a92e259120406ed9573e67d
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:38:41 +0300

    Added isMultiple.c
    Change-Id: I3eb0a25f086c4a811669bdc85002eee9dd17266e
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/isMultiple.c
@@ -0,0 +1,33 @@
+/*
+Задача isMultiple
+
+Условие задачи
+Считать с клавиатуры два целых числа, причем второе из них не равно нулю.
+Если первое кратно второму, вывести на экран yes, иначе вывести no.
+
+Пример ввода
+25 5
+Пример вывода
+yes
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int number, divisor;
+
+    scanf("%d %d", &number, &divisor);
+
+    if ((number % divisor) == 0)
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+
+    return 0;
+}
