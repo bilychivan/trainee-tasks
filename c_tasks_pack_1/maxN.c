2a69045be6e0097ebc6066cb6e14c396eab80967
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:39:33 +0300

    Added maxN.c
    Change-Id: I0f46357c9d80796bf406253669dcbf6df7b933f5
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/maxN.c
@@ -0,0 +1,43 @@
+/*
+Задача maxN
+
+Условие задачи
+Считать с клавиатуры целое положительное число - количество чисел в
+последовательности.
+Затем считать с клавиатуры заданное количество чисел и вывести наибольшее из
+них.
+Данная задача решается без массивов.
+
+Пример ввода
+5
+1 2 3 4 5
+Пример вывода
+5
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int numberOfInputs;
+    int max;
+
+    scanf("%u %d", &numberOfInputs, &max);
+
+    while (numberOfInputs > 1)
+    {
+        int temp;
+        scanf("%d", &temp);
+
+        if (max < temp)
+        {
+            max = temp;
+        }
+        numberOfInputs--;
+    }
+
+    printf("%i\n", max);
+
+    return 0;
+}
