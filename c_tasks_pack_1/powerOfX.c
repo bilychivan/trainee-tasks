0f967c4ad7c66bd040c9e97bbd40cb9aa5d5dc7b
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 18:03:39 +0300

    Added powerOfX.c
    Change-Id: Iedba0d3867b86b691a45b20b2d615d9518ecbde5
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/powerOfX.c
@@ -0,0 +1,33 @@
+/*
+Задача powerOfX
+
+Условие задачи
+Считать с клавиатуры целое основание и целый неотрицательный показатель. Вывести
+через пробел степени числа основания от нулевой до заданной.
+
+Пример ввода
+2 3
+Пример вывода
+1 2 4 8
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int base;
+    unsigned int number;
+    unsigned int m = 1;
+
+    scanf("%u %u", &base, &number);
+
+    for (unsigned int i = 0; i < number; i++)
+    {
+        printf("%i ", m);
+        m = m * base;
+    }
+    printf("%i\n", m);
+
+    return 0;
+}
