15fdfd01855b517eb4d617f33c430a6c0cdf3c09
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:40:03 +0300

    Added nextMultiple.c
    Change-Id: I7535059cb70c9b5f4f10d45d3f04bf85df6b142b
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/nextMultiple.c
@@ -0,0 +1,32 @@
+/*
+Задача nextMultiple
+
+Условие задачи
+Считать с клавиатуры два целых числа - делимое и делитель, причем делитель не равен нулю.
+Вывести на экран наименьшее число, кратное делителю, не меньшее делимого.
+Пример ввода
+15 6
+Пример вывода
+18
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int dividend, divider;
+
+    scanf("%d %d", &dividend, &divider);
+
+    if ((dividend % divider) != 0)
+    {
+        printf("%i\n", (dividend / divider) * divider + divider);
+    }
+    else
+    {
+        printf("%i\n", dividend);
+    }
+
+    return 0;
+}
