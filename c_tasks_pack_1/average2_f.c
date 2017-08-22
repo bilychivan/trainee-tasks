14cb178b0b39b3ce0c4498bbf3f625177a3c2d6a
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:31:30 +0300

    Added average2_f.c
    Change-Id: Idff5d6232ca59528cb9f3ec1222a2536cd7a2118
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/average2_f.c
@@ -0,0 +1,32 @@
+/*
+Задача average2()
+
+Условие задачи
+int average(int a, int b)
+Вернуть среднее арифметическое двух целых чисел. Использовать только тип int.
+Если среднее значение не является целым, то округлить его к ближайшему целому в
+сторону нуля.
+
+*/
+
+#include <stdio.h>
+
+int average(int a, int b)
+{
+    // source:
+    // http://www.hackersdelight.org/basics2.pdf
+    // Page 19, paragraph 2-5
+    int t = (a & b) + ((a ^ b) >> 1);
+    unsigned t_u = (unsigned)t;
+    return (t + ((t_u >> 31) & (a ^ b)));
+}
+
+int main()
+{
+    printf("Average of -10 and   5 is (should  -2): %d\n", average(-10, 5));
+    printf("Average of   6 and  15 is (should  10): %d\n", average(6, 15));
+    printf("Average of   6 and -15 is (should  -4): %d\n", average(6, -15));
+    printf("Average of  -6 and -15 is (should -10): %d\n", average(-6, -15));
+
+    return 0;
+}
