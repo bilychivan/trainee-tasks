759114821f69db3915c5a6f5b77ad8e3d436bf5c
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 18:03:33 +0300

    Added powerOf2.c
    Change-Id: Iee5f0a7ab505e288350cec2eb52809e47838a2d8
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/powerOf2.c
@@ -0,0 +1,34 @@
+/*
+Задача powerOf2
+
+Условие задачи
+Считать с клавиатуры целое неотрицательное число. Вывести через пробел степени
+числа 2 от нулевой до заданной. Использовать цикл.
+
+Пример ввода
+3
+Пример вывода
+1 2 4 8
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int number;
+    unsigned int m;
+    #define POWER 2
+
+    scanf("%u", &number);
+
+    m = 1;
+    for (unsigned int i = 0; i < number; i++)
+    {
+        printf("%i ", m);
+        m = m * POWER;
+    }
+    printf("%i\n", m);
+
+    return 0;
+}
