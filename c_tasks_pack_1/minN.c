466f1efcaff6501ab39b80dce40eaf921851c52b
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:39:40 +0300

    Added minN.c
    Change-Id: Ia1d4013fcc304bfaebd2bbf00fc9184a3577c308
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/minN.c
@@ -0,0 +1,40 @@
+/*
+Задача minN
+
+Условие задачи
+Считать с клавиатуры целое положительное число - количество чисел в
+последовательности.
+Затем считать с клавиатуры заданное количество чисел и вывести наименьшее из
+них.
+Данная задача решается без массивов.
+
+Пример ввода
+5
+1 2 3 4 5
+Пример вывода
+1
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int numberOfInputs, min;
+    scanf("%d %d", &numberOfInputs, &min);
+
+    while (numberOfInputs > 1)
+    {
+        int temp;
+        scanf("%d", &temp);
+
+        if (min > temp)
+        {
+            min = temp;
+        }
+        numberOfInputs--;
+    }
+
+    printf("%i\n", min);
+	return 0;
+}
