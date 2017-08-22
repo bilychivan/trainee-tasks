f81fe9ac3c0c580eb5e5f0c69841dbcda62447dc
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:37:36 +0300

    Added countdown.c
    Change-Id: I80e1dc267db2f856f1969ec700da5ba2b30f6b77
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/countdown.c
@@ -0,0 +1,30 @@
+/*
+Задача countdown
+
+Условие задачи
+Считать с клавиатуры целое неотрицательное число.
+Вывести в строку убывающий ряд чисел от заданного до нуля.
+
+Пример ввода
+7
+Пример вывода
+7 6 5 4 3 2 1 0
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	unsigned int number;
+
+	scanf("%u", &number);
+
+	while (number > 1)
+	{
+		printf("%u ", number);
+		number--;
+	}
+	printf("%u 0\n", number);
+	return 0;
+}
