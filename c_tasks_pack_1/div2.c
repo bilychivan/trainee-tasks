bcc337eb3c3497bea2de7c54a61db3e4ca301e25
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 17:38:09 +0300

    Added div2.c
    Change-Id: I6312b8bf89bc30a66182cfb897c651885a32ff58
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/div2.c
@@ -0,0 +1,33 @@
+/*
+Задача div2
+
+Условие задачи
+Считать min, max. Вывести в столбик чётные числа от min до max включительно.
+
+Пример ввода
+0 8
+Пример вывода
+0
+2
+4
+6
+8
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int start, max;
+    scanf("%d %d", &start, &max);
+
+    for ( ; start <= max; start++)
+    {
+        if ((start % 2) == 0)
+        {
+            printf("%d\n", start);
+        }
+    }
+	return 0;
+}
