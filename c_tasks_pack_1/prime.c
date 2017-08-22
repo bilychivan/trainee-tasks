8e95b2844a66c3b5a5043a2d9c73f8b4a5c6887d
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-22 18:03:52 +0300

    Added prime.c
    Change-Id: Id0c892a8eb18fecc3bdd933224d6d2f24a86d233
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_1/prime.c
@@ -0,0 +1,75 @@
+/*
+Задача prime
+
+Условие задачи
+Вывести N-ное по счёту простое число (см. Википедию).
+N считать с клавиатуры.
+Оптимизировать работу программы, насколько это возможно.
+В случае неопределенности ответа вывести -1.
+
+Пример ввода
+3
+Пример вывода
+5
+
+*/
+
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return 0;
+    }
+
+    if (n == 2)
+    {
+        return 1;
+    }
+
+    if ((n % 2) == 0)
+    {
+        return 0;
+    }
+
+    for (int i = 3; i < n; i += 2)
+    {
+        if ((n % i) == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    unsigned int target;
+    scanf("%d", &target);
+    if (target < 1)
+    {
+        printf("-1\n");
+    }
+    if (target == 1)
+    {
+        printf("2\n");
+        return 0;
+    }
+
+    unsigned int current = 1;
+    for (int i = 3;; i+=2)
+    {
+        if (isPrime(i))
+        {
+            current++;
+
+            if (current == target)
+            {
+                printf("%i\n", i);
+                break;
+            }
+        }
+    }
+    return 0;
+    }
