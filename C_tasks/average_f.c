dedc973321d14bb8bd7ead5e4fd038e42253ef29
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-23 16:37:58 +0300

    Added average_f.c
    Change-Id: I8d8e22373f08cf209da86f08c262b3adbeaa9d39
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ C_tasks/average_f.c
@@ -0,0 +1,30 @@
+/*
+Задача average()
+
+Условие задачи
+unsigned int average(unsigned int a, unsigned int b)
+Вернуть среднее арифметическое двух положительных целых чисел. Использовать
+только тип unsigned int.
+Если среднее значение не является целым, то округлить его к ближайшему целому в
+меньшую сторону.
+
+*/
+
+#include "stdio.h"
+
+unsigned int average(unsigned int a, unsigned int b)
+{
+	unsigned int remainder = a % 2 + b % 2;
+
+	return a / 2 + b /2 + remainder / 2;
+}
+
+int main()
+{
+	unsigned int value_1, value_2;
+	scanf("%u %u", &value_1, &value_2);
+
+	printf("%u\n", average(value_1, value_2));
+
+	return 0;
+}
