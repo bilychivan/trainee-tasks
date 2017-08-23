4336d642c96832676a535f5b4a2355738853d422
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-23 19:07:39 +0300

    Added maxN.c
    Change-Id: I78fe8adf697966e901d1d18835704ef88b3993b9
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ C_tasks/maxN.c
@@ -0,0 +1,41 @@
+/*
+Задача maxN
+
+Условие задачи
+Считать с клавиатуры целое положительное число - количество чисел в
+последовательности.
+Затем считать с клавиатуры заданное количество чисел и вывести наибольшее из
+них.
+Данная задача решается без массивов.
+
+Пример ввода
+5
+1 2 3 4 5
+Пример вывода
+5
+
+*/
+
+#include "stdio.h"
+
+int main()
+{
+	unsigned int quantity;
+	int compared_value, input_value;
+
+	scanf("%u %d", &quantity, &compared_value);
+
+	while (--quantity)
+	{
+		scanf("%d", &input_value);
+
+		if (compared_value < input_value)
+		{
+			compared_value = input_value;
+		}
+	}
+
+	printf("%d\n", compared_value);
+
+	return 0;
+}
