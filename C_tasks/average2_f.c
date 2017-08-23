0991a4aa891b98907a1c5ef7f03885f9eeaa95b2
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-23 16:26:12 +0300

    Added average2_f.c
    Change-Id: I463f2ca00aa8f76d3a136c1670e3251fe97e8be8
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ C_tasks/average2_f.c
@@ -0,0 +1,27 @@
+/*
+Задача average2()
+
+Условие задачи
+int average(int a, int b)
+Вернуть среднее арифметическое двух целых чисел. Использовать только тип int.
+Если среднее значение не является целым, то округлить его к ближайшему целому в
+сторону нуля.
+
+*/
+
+#include "stdio.h"
+
+int average(int a, int b)
+{
+	return a / 2 + b / 2 + (a % 2 + b % 2) / 2;
+}
+
+int main()
+{
+	int value_1, value_2;
+	scanf("%d %d", &value_1, &value_2);
+
+	printf("%d\n", average(value_1, value_2));
+
+	return 0;
+}
