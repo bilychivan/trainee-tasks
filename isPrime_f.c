20c010551360c2946fbc2e4bd19ca3e53c40162b
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-08-25 10:38:10 +0300

    Add new file isPrime_f.c
    Change-Id: Ic104a945f3cde9741774b076ecbb66539e8dfb78
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ isPrime_f.c
@@ -0,0 +1,27 @@
+/*
+Задача isPrime()
+
+Условие задачи
+Написать функцию:
+int isPrime(int n)
+
+Проверить, является ли данное число простым.
+
+*/
+
+#include <stdio.h>
+
+#define TRUE	1
+#define FALSE	0
+
+int isPrime(int n) {
+	if (n > 1) { 
+		for (int i = 2; i <= n / 2; i++) {
+			if ((n % i) == 0) {
+				return FALSE;
+			}
+		}
+	}
+
+	return TRUE;
+}
