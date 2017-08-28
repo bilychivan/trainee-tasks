090330d1aceb8507edf7f43a6961f1b8efbb96a3
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-08-28 19:48:29 +0300

    tasks_pack_2: Add new file c_tasks_pack_2/sequenceLen.c
    Change-Id: I51d01c4dd59eebc19c51b92f0e9f8972291dbe2f
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c_tasks_pack_2/sequenceLen.c
@@ -0,0 +1,45 @@
+/*
+Задача sequenceLen
+Условие задачи
+Считать из файла task.in последовательность чисел размером не более 100
+элементов.
+Посчитать длину последовательности и вывести ее в task.out.
+
+Пример ввода
+10 20 30 40 50 60 70 80 90 100
+Пример вывода
+10
+
+*/
+
+#include <stdio.h>
+
+#define MAXLEN 100
+#define FILEIN "task.in"
+#define FILEOUT "task.out"
+
+int main(void)
+{
+    FILE *fileIn, *fileOut;
+    int len;
+    int n;
+
+    if ((fileIn = fopen(FILEIN, "r")) == NULL)
+    {
+        return 1;
+    }
+    if ((fileOut = fopen(FILEOUT, "w")) == NULL)
+    {
+        return 1;
+    }
+
+    for (len = 0; fscanf(fileIn,"%d", &n) != EOF && len < MAXLEN; len++)
+    {
+    }
+    fprintf(fileOut, "%d\n", len);
+
+    fclose(fileIn);
+    fclose(fileOut);
+
+    return 0;
+}
