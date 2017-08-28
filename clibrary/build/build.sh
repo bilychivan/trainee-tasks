53f97c163710b5059de344e05c7f0ba11a36421b
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-28 16:30:36 +0300

    library: Adding simple C library (with demo)
    Change-Id: Ie62e037ba798103236f81e57f303b97a16864ce3
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ clibrary/build/build.sh
@@ -0,0 +1,3 @@
+cmake ../
+make
+./demo/lyslib_demo
---
+++ clibrary/library/include/lyslib.h
@@ -0,0 +1,14 @@
+#ifndef _LYSLIB_H_
+#define _LYSLIB_H_
+
+    extern void fubar();
+
+    extern void asap();
+
+    extern void lol();
+
+    extern void rofl();
+
+    extern void whoami();
+
+#endif
