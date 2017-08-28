b76adc4a25bfd6b5cdcc20c13512532ff5a5e0ef
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-28 16:30:36 +0300

    library: Adding simple C library (with demo)
    Change-Id: Ie62e037ba798103236f81e57f303b97a16864ce3
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ clibrary/library/src/lyslib.c
@@ -0,0 +1,32 @@
+#include "../include/lyslib.h"
+#include <stdio.h>
+
+void fubar()
+{
+    printf("f*** up beyond any recognition\n");
+    return;
+}
+
+void asap()
+{
+    printf("as fast as possible\n");
+    return;
+}
+
+void lol()
+{
+    printf("laughing out loud\n");
+    return;
+}
+
+void rofl()
+{
+    printf("rolling on the floor laughing\n");
+    return;
+}
+
+void whoami()
+{
+    printf("simple c library\n");
+    return;
+}
