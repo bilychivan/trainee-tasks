82b0de99852c8f27d079368780c83dac03038bdf
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-28 16:30:36 +0300

    library: Adding simple C library (with demo)
    Change-Id: Ie62e037ba798103236f81e57f303b97a16864ce3
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ clibrary/demo/demo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../library/include/lyslib.h"
+
+int main()
+{
+    printf("\n-----\n Simple C library demo program\n-----\n");
+    fubar();
+    asap();
+    lol();
+    rofl();
+    whoami();
+    return 0;
+}
