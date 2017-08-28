3d0972a5aa45cf011ac68a26a730a7700d62ca84
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-08-28 19:48:29 +0300

    tasks_pack_2: Add new file c_tasks_pack_2/arrayExchange_f.c
    Change-Id: Iadfde9082d3e932db94d065ea080cef7ece0bde5
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c_tasks_pack_2/arrayExchange_f.c
@@ -0,0 +1,44 @@
+/*
+Задача arrayExchange()
+
+Условие задачи
+Написать функцию
+void arrayExchange(int array[], int len)
+
+Попарно обменять местами все четные элементы массива с нечетными, начиная с
+нулевого.
+a[0] <-> a[1]   a[2] <-> a[3]   .....
+Если последний элемент не имеет пары — не менять его значение.
+
+Запрещено использование дополнительного массива.
+
+*/
+
+#include <stdio.h>
+
+void arrayExchange(int array[], int len)
+{
+    int swap;
+    int maxLen = len - 1;;
+
+    for (int i = 0; i < maxLen; i += 2)
+    {
+        swap = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = swap;
+    }
+}
+
+int main(void)
+{
+    int array[9] = {0,1,2,3,4,5,6,7,8};
+
+    arrayExchange(array, 9);
+
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+
+    return 0;
+}
