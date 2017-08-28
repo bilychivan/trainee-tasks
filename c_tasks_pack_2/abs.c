3979a7034deca8259c4064a04ddd1d9f5ef6a3bd
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-28 15:14:49 +0300

    pack2: Added abs.c
    Change-Id: I7c6940617dceb3385812f0096ba568bda973917e
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_2/abs.c
@@ -0,0 +1,67 @@
+/*
+Задача abs
+
+Условие задачи
+В файле task.in дана длина последовательности чисел (число больше нуля) и сама
+последовательность.
+Прочитать последовательность в массив.
+Найти все отрицательные числа и заменить их положительными, равными по модулю.
+Результирующий массив вывести в task.out.
+
+Пример ввода
+10
+10 -20 30 40 -50 -60 70 80 90 -100
+Пример вывода
+10 20 30 40 50 60 70 80 90 100
+
+*/
+
+#include <stdio.h>
+
+int readInputSize(FILE* inputFile)
+{
+    int temp;
+
+    fscanf(inputFile, "%d", &temp);
+
+    return temp;
+}
+
+void processInput(FILE* inputFile, int inputLength, int inputNumbers[])
+{
+    for (int i = 0; i < inputLength; i++)
+    {
+        fscanf(inputFile, "%d", &inputNumbers[i]);
+
+        if (inputNumbers[i] < 0)
+        {
+            inputNumbers[i] *= -1;
+        }
+    }
+}
+
+int main()
+{
+    FILE* inputFile = fopen("task.in", "r");
+    FILE* outputFile;
+    int inputLength = readInputSize(inputFile);
+    int inputNumbers[inputLength];
+    int size;
+
+    processInput(inputFile, inputLength, inputNumbers);
+
+    fclose(inputFile);
+
+    outputFile = fopen("task.out", "w");
+
+    size = inputLength - 1;
+    for (unsigned int i = 0; i < size; i++)
+    {
+        fprintf(outputFile, "%d ", inputNumbers[i]);
+    }
+    fprintf(outputFile, "%d\n", inputNumbers[size]);
+
+    fclose(outputFile);
+
+    return 0;
+}
