c9628211440e3c4f2da636a749dd654a2bc58ca9
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-28 15:17:30 +0300

    pack2: Added base2.c
    Change-Id: I15441a6cc78825a50c4ac9e4f7f0d0ef19838123
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c_tasks_pack_2/base2.c
@@ -0,0 +1,59 @@
+/*
+Задача base2
+
+Условие задачи
+Считать с клавиатуры целое число в десятичной системе счисления и основание
+новой системы счисления (целое число от 2 до 36).
+Вывести в консоль число, записанное в новой системе счисления.
+В качестве цифр, превышающих 9, использовать заглавные буквы латинского
+алфавита.
+Для решения можно пользоваться только циклами. Задача решается без массивов.
+Использовать только тип int.
+
+Пример ввода
+-255 16
+Пример вывода
+-FF
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    const int MAX_DIGIT = 9;
+    const int CONVERT_DIGIT_TO_LETTER = 7;
+    int input;
+    int base;
+    int maxFactor = 1;
+
+    scanf("%d %u", &input, &base);
+
+    if (input < 0)
+    {
+        input *= -1;
+        printf("-");
+    }
+
+    for (int temp = input; temp >= base; temp /= base)
+    {
+        maxFactor *= base;
+    }
+
+    for (; maxFactor > 0; maxFactor /= base)
+    {
+        int offset = input / maxFactor;
+
+        if (offset > MAX_DIGIT)
+        {
+            offset += CONVERT_DIGIT_TO_LETTER;
+        }
+
+        printf("%c", '0' + offset);
+
+        input %= maxFactor;
+    }
+    printf("\n");
+
+    return 0;
+}
