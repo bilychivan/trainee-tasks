d90797deb1b0a0a61266e151d93adf3cff64d9ff
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-28 16:30:36 +0300

    library: Adding simple C library (with demo)
    Change-Id: Ie62e037ba798103236f81e57f303b97a16864ce3
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ clibrary/build/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cmake ../
+make
+./demo/lyslib_demo
---
+++ clibrary/library/src/lyslib.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "lyslib.h"
+
+void secret()
+{
+    printf("secret code is 42\n");
+    return;
+}
+
+void asap()
+{
+    printf("as fast as possible\n");
+    return;
+}
+
+void lol()
+{
+    printf("laughing out loud\n");
+    return;
+}
+
+void rofl()
+{
+    printf("rolling on the floor laughing\n");
+    return;
+}
+
+void whoami()
+{
+    printf("simple c library\n");
+    return;
+}
