da82d4772b46e437b863c3fd4c22818fce94bb8d
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-08-29 17:28:18 +0300

    SimpleLib: add new impl of Makefile
    Change-Id: I45c03ca01b34bcc258fbb65e9555b49b5868b176
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c_tasks_pack_2/simpleLib/includes/mylib.h
@@ -0,0 +1,11 @@
+#ifndef MYLIB_H
+# define MYLIBFT_H
+
+# include <stdio.h>
+
+int abs(int n);
+int decToBase(int num, int base);
+void sliceReverse(int array[], int lo, int hi);
+char toLower(char c);
+
+#endif
---
+++ c_tasks_pack_2/simpleLib/lib/tolower.c
@@ -0,0 +1,10 @@
+#include "mylib.h"
+
+char toLower(char c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        c += 32;
+    }
+    return c;
+}
