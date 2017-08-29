c962e6e5de0211b6828c53b117d038926b833b13
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-08-29 14:10:40 +0300

    pack2: Fixed sliceReverse_f.c
    Change-Id: Id583a15f934828fbbf0b4b9a45595cda19cd7a5a
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

--- c_tasks_pack_2/sliceReverse_f.c
+++ c_tasks_pack_2/sliceReverse_f.c
@@ -18,26 +18,8 @@ void sliceReverse(int array[], int lo, int hi)
 
     for (; low < high; low++, high--)
     {
-        char temp = *low;
-        *low      = *high;
-        *high     = temp;
+        int temp = *low;
+        *low = *high;
+        *high = temp;
     }
-}
-
-void test(int array[], int lo, int hi, int len)
-{
-    sliceReverse(array, lo, hi);
-    for (unsigned int i = 0; i < len; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-}
-
-int main()
-{
-    int array1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    test(array1, 2, 7, 10);
-
-    return 0;
 }
