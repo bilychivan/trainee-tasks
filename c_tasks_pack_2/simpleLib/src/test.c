3d9968dcff9bad4c219eb8a4cbfb5efbc8b796fb
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-08-29 17:28:18 +0300

    SimpleLib: remove old library
    Change-Id: I45c03ca01b34bcc258fbb65e9555b49b5868b176
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c_tasks_pack_2/simpleLib/src/test.c
@@ -0,0 +1,9 @@
+
+// #include <stdio.h>
+#include "mylib.h"
+
+int main(void)
+{
+    printf("abc: from \"%d\" to \"%d\"\n", -19, abs(-19));
+    printf("toLower: from \"%c\" to \"%c\"\n",'A', toLower('A'));
+}
