3ab58452311a6f8d3e561ea95cb4763f49cd0265
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-30 16:06:57 +0300

    pack3: Added arrayShift_f.c
    Change-Id: I99bf960c16352e4136a598176df6b0b1ad0e8223
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c_tasks_3/arrayShift_f.c
@@ -0,0 +1,87 @@
+/*
+Задача arrayShift()
+
+Условие задачи
+Написать функцию:
+void arrayShift(int array[], int size, int shift)
+Реализовать циклический сдвиг массива на shift элементов вправо. Максимизировать
+производительность при оптимальном использовании памяти.
+Требуемая сложность: O(N)
+
+*/
+
+#include <stdio.h>
+
+#define SIZE 10
+
+void arrayShift(int array[], int size, int shift)
+{
+    if (shift < 0)
+    {
+        shift += size;
+    }
+
+    shift %= size;
+
+    int temp[shift];
+
+    for (int i = 0, offset = size - shift; i < shift; ++i)
+    {
+        temp[i] = array[i + offset];
+    }
+
+    for (int i = size - 1; i >= shift; --i)
+    {
+        array[i] = array[i - shift];
+    }
+
+    for (int i = 0; i < shift; ++i)
+    {
+        array[i] = temp[i];
+    }
+}
+
+void display(const int array[], int len)
+{
+    if (len < 1)
+    {
+        printf("\n");
+
+        return;
+    }
+
+    int size = len - 1;
+
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("%d\n", array[size]);
+}
+
+void test(int array[], int size, int shift)
+{
+    display(array, size);
+
+    arrayShift(array, size, shift);
+
+    display(array, size);
+
+    printf("--------------------\n");
+}
+
+int main()
+{
+    int numbers[] =
+    {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+
+    test(numbers, SIZE, 1);
+    test(numbers, SIZE, 2);
+    test(numbers, SIZE, 3);
+    test(numbers, SIZE, 12);
+    test(numbers, SIZE, -1);
+
+    return 0;
+}
