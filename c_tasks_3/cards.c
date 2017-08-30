c1480d0395a6ae7356b13c15891b49b13f361633
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-30 16:54:08 +0300

    pack3: Added cards.c
    Change-Id: If58a087c0e6eb862f97a6ea71279a32e92b9ee6f
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c_tasks_3/cards.c
@@ -0,0 +1,54 @@
+/*
+Игральные карты
+
+Создать struct для игры в карты. Понадобится пара enum-ов.
+Обеспечить соответствие между достоинством карты и его числовым эквивалентом,
+т.е., к примеру, числовой эквивалент "девятки" должен быть равен 9.
+*/
+
+#include <stdio.h>
+
+typedef enum
+{
+    SIX = 6,
+    SEVEN,
+    EIGHT,
+    NINE,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+} Value;
+
+typedef enum
+{
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+} Suit;
+
+
+typedef struct
+{
+    Value val;
+    Suit suit;
+} Card;
+
+void test()
+{
+    Card cards[] =
+    {
+        {SIX, HEARTS},{ACE, DIAMONDS}
+    };
+
+    printf("%d\n", cards[0].val);
+    printf("%d\n", cards[1].val);
+}
+
+int main()
+{
+    test();
+
+    return 0;
+}
