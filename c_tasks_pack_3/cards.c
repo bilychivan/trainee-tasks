662403c0656a1162b210ab0e64602a12a6e7c70e
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-08-30 18:01:03 +0300

    c_tasks_pack_3: Add new file cards.c
    Change-Id: Ifd0f3ee0bba16f87a7e5b243f9dd082e22a2d52f
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c_tasks_pack_3/cards.c
@@ -0,0 +1,35 @@
+/*
+Игральные карты
+
+Создать struct для игры в карты. Понадобится пара enum-ов.
+Обеспечить соответствие между достоинством карты и его числовым эквивалентом,
+т.е., к примеру, числовой эквивалент "девятки" должен быть равен 9.
+*/
+
+typedef enum {
+    clubs,
+    diamonds,
+    hearts,
+    spades
+} Suit;
+
+typedef enum {
+    ace,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    jack,
+    queen,
+    king
+} Card;
+
+typedef struct {
+    Suit suit;
+    Card card;
+} PlayingCard;
