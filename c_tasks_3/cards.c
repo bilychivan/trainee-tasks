f21828e2daeb44f1f2567aed8ef9e9efa2378ee4
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-30 16:54:08 +0300

    pack3: Added cards.c
    Change-Id: If58a087c0e6eb862f97a6ea71279a32e92b9ee6f
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c_tasks_3/cards.c
@@ -0,0 +1,76 @@
+/*
+Игральные карты
+
+Создать struct для игры в карты. Понадобится пара enum-ов.
+Обеспечить соответствие между достоинством карты и его числовым эквивалентом,
+т.е., к примеру, числовой эквивалент "девятки" должен быть равен 9.
+*/
+
+#include <stdio.h>
+
+typedef enum
+{
+    ACE = 1,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    JACK,
+    QUEEN,
+    KING,
+} Name;
+
+typedef enum
+{
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+} Suit;
+
+typedef struct
+{
+    Name name;
+    Suit suit;
+} Card;
+
+const char* cardNames[] =
+{
+    "ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "JACK", "QUEEN", "KING"
+};
+
+const char* nameToString(Card card)
+{
+    return cardNames[card.name - 1];
+}
+
+int nameToValue(Card card)
+{
+    return card.name;
+}
+
+void test()
+{
+    const Card cards[] =
+    {
+        {TWO, HEARTS}, {ACE, DIAMONDS}, {JACK, SPADES}, {KING, CLUBS}
+    };
+
+    const int size = sizeof(cards) / sizeof(Card);
+
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%s = %d\n", nameToString(cards[i]), nameToValue(cards[i]));
+    }
+}
+
+int main()
+{
+    test();
+
+    return 0;
+}
