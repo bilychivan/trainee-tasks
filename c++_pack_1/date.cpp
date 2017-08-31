5b9e9fa0da7243dc4b0d4f4287fd77cb1f6d612b
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-31 15:40:53 +0300

    c++_pack_1: Added date
    Change-Id: I4759d160d1cb027588029e09337dd3f1d307ae68
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c++_pack_1/date.cpp
@@ -0,0 +1,66 @@
+#include "date.h"
+
+const std::array<int, Date::DECEMBER> Date::dayQuantity
+{
+    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+Date::Date(int day, int month, int year)
+    : mDay(day)
+    , mMonth(month)
+    , mYear(year)
+{
+    checkArguments();
+}
+
+int Date::getDay() const
+{
+    return mDay;
+}
+
+int Date::getMonth() const
+{
+    return mMonth;
+}
+
+int Date::getYear() const
+{
+    return mYear;
+}
+
+void Date::checkArguments() const
+{
+    if (mMonth < JANUARY || mMonth > DECEMBER)
+    {
+        throw DateException("wrong month was forwarded: " + std::to_string(mMonth));
+    }
+    else if (mDay < JANUARY || mDay > getDayQuantity())
+    {
+        throw DateException("wrong day was forwarded: " + std::to_string(mDay));
+    }
+}
+
+int Date::getDayQuantity() const
+{
+    int quantity = dayQuantity[mMonth - 1];
+
+    if (mMonth == FEBRUARY && ((mYear % LEAP1 == 0 && mYear % NOT_LEAP1) || mYear % LEAP2 == 0))
+    {
+        quantity += 1;
+    }
+
+    return quantity;
+}
+
+std::ostream& operator<<(std::ostream& os, const Date& date)
+{
+    std::cout << "Date: " << date.getDay() << '.' << date.getMonth() << '.' << date.getYear();
+
+    return os;
+}
+
+DateException::DateException(const std::string& message)
+    : std::invalid_argument(message)
+{
+
+}
