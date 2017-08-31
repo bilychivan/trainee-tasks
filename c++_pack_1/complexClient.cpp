52a70e0a028ffc681b6936dc1ff00af0a80afe81
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-31 13:24:43 +0300

    c++_pack_1: Added complex
    Change-Id: I8992daec9e994c29e5e83c167886e42708870f40
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c++_pack_1/complexClient.cpp
@@ -0,0 +1,79 @@
+#include "complex.h"
+
+void test()
+{
+    Complex a(3.0, 2.0), b(1.0, 4.0), c(1.0, 1.0), d(2.0, 2.0);
+
+    std::cout << "a: " << a << "\nb: " << b << "\nc: " << c << "\nd: " << d << std::endl;
+
+    std::cout << "a.getRe() a.getIm(): " << a.getRe() << ' ' << a.getIm() << std::endl;
+    
+    std::cout << "a + b = " << a + b << std::endl;
+
+    std::cout << "a - b = " << a - b << std::endl;
+    
+    std::cout << "a * b = " << a * b << std::endl;
+
+    std::cout << "a.abs() = " << a.abs() << std::endl;
+    
+    a += c;
+    b -= c;
+    c *= d;
+
+    std::cout << "a += c: " << a << std::endl;
+
+    std::cout << "b -= c: " << b << std::endl;
+
+    std::cout << "c *= d: " << c << std::endl;
+
+    std::cout << "a == a: ";
+
+    if (a == a)
+    {
+        std::cout << "yes\n";
+    }
+    else
+    {
+        std::cout << "no\n";
+    }
+
+    std::cout << "a == b: ";
+
+    if (a == b)
+    {
+        std::cout << "yes\n";
+    }
+    else
+    {
+        std::cout << "no\n";
+    }
+
+    std::cout << "a != a: ";
+
+    if (a != a)
+    {
+        std::cout << "yes\n";
+    }
+    else
+    {
+        std::cout << "no\n";
+    }
+
+    std::cout << "a != b: ";
+
+    if (a != b)
+    {
+        std::cout << "yes\n";
+    }
+    else
+    {
+        std::cout << "no\n";
+    }
+}
+
+int main()
+{
+    test();
+
+    return 0;
+}
