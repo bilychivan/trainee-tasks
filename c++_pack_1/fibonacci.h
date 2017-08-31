bc6ce6901ee92c81491e8e1a64fb1b5580d7152e
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-31 18:10:16 +0300

    c++: pack1: Added fibonacci
    Change-Id: Ic94d4d279c6948f610892193f8808a05dec022d5
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c++_pack_1/fibonacci.h
@@ -0,0 +1,48 @@
+#ifndef FIBONACCI_CLASS_H
+#define FIBONACCI_CLASS_H
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+class Fibonacci
+{
+public:
+    Fibonacci(int index, int size);
+
+    int getIndex() const;
+
+    void next();
+    void prev();
+
+    int value() const;
+
+    bool over() const;
+    bool first() const;
+
+    int operator++();
+    int operator++(int);
+
+    int operator--();
+    int operator--(int);
+
+    int operator*();
+
+private:
+    int fib(int n) const;
+
+    int mCurrent;
+    int mIndex;
+    const int mInitialIndex;
+    const int mLastIndex;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fibonacci& f);
+
+class FibonacciException : public std::runtime_error
+{
+public:
+    FibonacciException(const std::string& message);
+};
+
+#endif // FIBONACCI_CLASS_H
