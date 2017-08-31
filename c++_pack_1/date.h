36eccdab5d2baf6bcdb96c8ab43c2019e5d186a1
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-08-31 15:40:53 +0300

    c++_pack_1: Added date
    Change-Id: I4759d160d1cb027588029e09337dd3f1d307ae68
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c++_pack_1/date.h
@@ -0,0 +1,56 @@
+#ifndef DATE_CLASS_H
+#define DATE_CLASS_H
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+class Date
+{
+public:
+    enum
+    {
+        JANUARY = 1,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER,
+        LEAP1 = 4,
+        NOT_LEAP1 = 100,
+        LEAP2 = 400
+    };
+
+    Date(int day, int month, int year);
+
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+
+private:
+    void checkArguments() const;
+    int getDayQuantity() const;
+
+    const int mDay;
+    const int mMonth;
+    const int mYear;
+
+    static const std::array<int, DECEMBER> dayQuantity;
+};
+
+std::ostream& operator<<(std::ostream& os, const Date& date);
+
+class DateException : public std::invalid_argument
+{
+public:
+    explicit DateException(const std::string& message);
+};
+
+#endif // DATE_CLASS_H
