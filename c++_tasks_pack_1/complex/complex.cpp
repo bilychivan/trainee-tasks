9f310c369c5ba7923b087d8bbeb68cc9b9cbaa36
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-09-01 16:33:59 +0300

    C++: pack1: Add Complex
    Change-Id: I09fb77599a1dc30602fd7b93a1922a2eea253bc5
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c++_tasks_pack_1/complex/complex.cpp
@@ -0,0 +1,91 @@
+#include "complex.h"
+
+Complex::Complex() : re(0), im(0) {}
+
+Complex::Complex(double re, double im) : re(re), im(im) {}
+
+double Complex::getRe() const
+{
+    return re;
+}
+
+double Complex::getIm() const
+{
+    return im;
+}
+
+double Complex::abs() const
+{
+    return hypot(re, im);
+}
+
+void Complex::operator+=(const Complex& other)
+{
+    re += other.re;
+    im += other.im;
+}
+
+void Complex::operator-=(const Complex& other)
+{
+    re -= other.re;
+    im -= other.im;
+}
+
+void Complex::operator*=(const Complex& other)
+{
+    double newRe = this->re;
+
+    this->re = (newRe * other.re) - (this->im * other.im);
+    this->im = (newRe * other.im) + (this->im * other.re);
+    this->re = newRe;
+}
+
+Complex Complex::operator+(const Complex& other) const
+{
+    Complex result = *this;
+
+    result += other;
+
+    return result;
+}
+
+Complex Complex::operator-(const Complex& other) const
+{
+    Complex result;
+
+    result.im = im - other.im;
+    result.re = re - other.re;
+
+    return result;
+}
+
+Complex Complex::operator*(const Complex& other) const
+{
+    Complex result;
+
+    result *= other;
+
+    return result;
+}
+
+bool Complex::operator==(const Complex& other) const
+{
+    return im == other.im && re == other.re;
+}
+
+bool Complex::operator!=(const Complex& other) const
+{
+    return !operator==(other);
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& comp)
+{
+    if (comp.getIm() < 0)
+    {
+        return os << comp.getRe() << comp.getIm();
+    }
+    else
+    {
+        return os << comp.getRe() << '+' << comp.getIm();
+    }
+}
