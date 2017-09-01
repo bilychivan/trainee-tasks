f6e57216d74efc0db774758875d148863b01140b
Alexander Lysunets <oleksandr.lysunets@globallogic.com>
2017-09-01 17:42:56 +0300

    c++: pack1: Add Prime
    Change-Id: Ie6b858fd93216393860e903e05e27d7363719df5
    Signed-off-by: Alexander Lysunets <oleksandr.lysunets@globallogic.com>

---
+++ c++_tasks_pack_1/prime.h
@@ -0,0 +1,56 @@
+#ifndef PRIME_H
+#define PRIME_H
+
+#include <iostream>
+#include <vector>
+
+class Prime
+{
+    public:
+        Prime(const int argFirstIndex, const int argLength);
+        int getIndex() const;
+        void next();
+        void prev();
+        int value();
+        bool last() const;
+        bool first() const;
+        int operator++();
+        int operator++(int);
+        int operator--();
+        int operator--(int);
+        int operator*();
+
+    private:
+        struct PrimeCachedElement
+        {
+            bool available;
+            unsigned int value;
+        };
+
+        struct PrimeCacheHead
+        {
+            unsigned int startValue;
+            unsigned int startIndex;
+        };
+
+        int index;
+        int minIndex;
+        int maxIndex;
+
+        static std::vector<PrimeCachedElement> cache;
+        static PrimeCacheHead cacheHead;
+
+        friend void fillCache(const Prime *prime, unsigned int index, unsigned int value);
+        friend unsigned int getNthPrime(Prime const *prime, unsigned int n);
+        friend unsigned int calculateNthPrime(Prime const *prime, unsigned int n);
+};
+
+std::ostream& operator<<(std::ostream&, Prime&);
+
+class PrimeException : public std::out_of_range
+{
+    public:
+        PrimeException(const char*);
+};
+
+#endif // PRIME_H
