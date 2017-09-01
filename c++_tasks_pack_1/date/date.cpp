09c1950a77860ca9d1ceab2c109bb586d440f516
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-09-01 16:47:10 +0300

    C++: pack1: Add new file date
    Change-Id: I83f5e4e018ef52a5ff232c7101779d5724282f3c
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c++_tasks_pack_1/date/date.cpp
@@ -0,0 +1,99 @@
+#include "date.h"
+
+using namespace std;
+
+Date::Date(int nDate, int nMonth, int nYear) :
+           day(nDate), month(nMonth), year(nYear)
+{
+    checkDate();
+};
+
+int Date::getDay() const
+{
+    return day;
+}
+
+int Date::getMonths() const
+{
+    return month;
+}
+
+int Date::getYear() const
+{
+    return year;
+}
+
+bool Date::checkDay()
+{
+    if (day < 1 || day > 31 || day > maxDay[month - 1])
+    {
+        return false;
+    }
+
+    if (month == FEBRUARY && day == LEAPDAY)
+    {
+        return ((!(year % LEAPYEAR) && (year % NOTLEAPYEAR)) || (!(year % LEAPYEAROLD)));
+    }
+
+    return true;
+}
+
+void Date::checkDate()
+{
+    if (year < 0 || month < JANUARY || month > DECEMBER)
+    {
+        throw DateException("Exc: wrong");
+    }
+    if (!checkDay())
+    {
+        throw DateException("Exc: wrong");
+    }
+}
+
+
+
+std::ostream& operator<<(std::ostream& os, Date& date)  
+{
+    os << date.getDay() << ' ' << date.getMonths() << ' ' << date.getYear();
+
+    return os;
+}
+
+int Date::maxDay[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int main()
+{
+    Date today(15, 5, 2016);
+    Date happybirthday(17, 1, 1989);
+    Date blackDay(29, 2, 1600);
+
+    cout << today << endl << happybirthday << endl << blackDay << endl;
+
+    try {
+        Date date(99, 8, 2013);
+    } catch (DateException obj) {
+        cout << obj.what() << endl;
+    }
+    try {
+        Date date(29, 99, 2013);
+    } catch (DateException obj) {
+        cout << obj.what() << endl;
+    }
+    try {
+        Date date(29, 8, 0);
+    } catch (DateException obj) {
+        cout << obj.what() << endl;
+    }
+    try {
+        Date date(29, 2, 1700);
+    } catch (DateException obj) {
+        cout << obj.what() << endl;
+    }
+    try {
+        Date date(29, 2, 1900);
+    } catch (DateException obj) {
+        cout << obj.what() << endl;
+    }
+
+    return 0;
+}
