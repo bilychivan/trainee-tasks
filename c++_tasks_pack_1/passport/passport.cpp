2e3c667f23e243ac110dfa5bad0e10bec18afd12
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-09-04 12:59:50 +0300

    C++: pack1: Fix the Passport file
    Change-Id: I14e12cdc56137a2c6fee89d76321cb425f17d9f4
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c++_tasks_pack_1/passport/passport.cpp
@@ -0,0 +1,101 @@
+#include "passport.h"
+
+Passport::Passport(std::string inputFirstName, std::string inputLastName, Date inputDate) :
+                    firstName(inputFirstName), lastName(inputLastName), date(inputDate)
+{
+    setNumber();
+    series.push_back(firstLetter);
+    series.push_back(secondLetter);
+}
+
+Passport::Passport(std::string inputFirstName, std::string inputLastName, Date inputDate, std::string inputSeries) :
+                    firstName(inputFirstName), lastName(inputLastName), date(inputDate), series(inputSeries)
+{
+    setNumber();
+}
+
+void Passport::setNumber()
+{
+    if (globalNumber < TOTAL_NUMBERS)
+    {
+        globalNumber += 1;
+        number = globalNumber;
+
+        return;
+    }
+    if (secondLetter < 'Z')
+    {
+        secondLetter += 1;
+        globalNumber = 1;
+        number = globalNumber;
+
+        return;
+    }
+    if (firstLetter < 'Z')
+    {
+        secondLetter = 'A';
+        firstLetter += 1;
+        globalNumber = 1;
+        number = globalNumber;
+
+        return;
+    }
+    throw PassportException("Free passport values are over");
+}
+
+const std::string& Passport::getSeries() const
+{
+    return series;
+}
+
+int Passport::getNumber() const
+{
+    return number;
+}
+
+const std::string& Passport::getFirstName() const
+{
+    return firstName;
+}
+
+const std::string& Passport::getLastName() const
+{
+    return lastName;
+}
+
+const Date& Passport::getDate() const
+{
+    return date;
+}
+
+std::ostream& operator<<(std::ostream& os, const Passport& passport)
+{
+    os << passport.getSeries() << std::setfill('0') << std::setw(6) << passport.getNumber() << ' ' << passport.getFirstName() << ' ' << passport.getLastName() << " Date of receiving:"
+       << passport.getDate().getDay() << ' ' << passport.getDate().getMonths() << ' ' << passport.getDate().getYear();
+
+    return os;
+}
+
+char Passport::firstLetter = 'A';
+char Passport::secondLetter = 'A';
+int Passport::globalNumber = 0;
+
+int main()
+{
+    Date date(11, 6, 2000);
+
+    for (int i = 0; i < 10; i++)
+    {
+        Passport prog1("Name", "Surname", date);
+        std::cout << prog1 <<  std::endl;
+    }
+    Passport test = Passport("Name", "Surname", date, "ZZ");
+    for (int i = 0; i < 10; i++)
+    {
+        Passport prog1("Name", "Surname", date);
+        std::cout << prog1 <<  std::endl;
+    }
+    std::cout << test <<  std::endl;
+
+    return 0;
+}
