91ead07c14c41a83ac9f84e37079580d8f7bf7fe
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2017-09-08 16:48:49 +0300

    c++: pack2: Added Shop
    Change-Id: I10132ef8d0db11ff67b0e92a19ede6c4940fdb6b
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

---
+++ c++_pack_2/shop/Include/attributes.h
@@ -0,0 +1,15 @@
+#ifndef ATTRIBUTES_H
+#define ATTRIBUTES_H
+
+namespace SHOP_CLASSES
+{
+    enum TYPE
+    {
+        CATEGORY,
+        ITEM,
+        ORDER,
+        CUSTOMER
+    };
+}
+
+#endif
---
+++ c++_pack_2/shop/Source/item.cpp
@@ -0,0 +1,89 @@
+#include "item.h"
+#include "category.h"
+#include "order.h"
+
+#include <algorithm>
+
+Item::Item(const std::string& name, Category* category)
+    : Base(name), mCategory(category), mOrders()
+{
+    category->appendItem(this);
+
+    std::cout << "    Item " << this << " constr\n";
+}
+
+Item::~Item()
+{
+    std::cout << "    Item " << this << " destr | ";
+
+    mCategory->removeItem(this);
+
+    for (Order* order: mOrders)
+    {
+        order->removeItem(this);
+
+        if (order->getItems().empty())
+        {
+            order->notify(SHOP_CLASSES::CUSTOMER);
+        }
+    }
+}
+
+void Item::setCategory(Category* category)
+{
+    mCategory->removeItem(this);
+
+    mCategory = category;
+
+    mCategory->appendItem(this);
+
+    std::cout << "Item " << this << " set category: " << category << std::endl;
+}
+
+const Category* Item::getCategory() const
+{
+    return mCategory;
+}
+
+void Item::displayCategory() const
+{
+    std::cout << "Item " << this << " has category: " << mCategory << std::endl;
+}
+
+void Item::appendOrder(Order* order)
+{
+    if (std::find(mOrders.begin(), mOrders.end(), order) != mOrders.end())
+    {
+        throw ItemException("order already added: " + order->getName());
+    }
+
+    mOrders.push_back(order);
+}
+
+void Item::removeOrder(Order* order)
+{
+    auto it = std::find(mOrders.begin(), mOrders.end(), order);
+
+    if (it == mOrders.end())
+    {
+        throw CategoryException("order doesn't present in item: " + order->getName());
+    }
+
+    mOrders.erase(it);
+}
+
+const std::list<Order*>& Item::getOrders() const
+{
+    return mOrders;
+}
+
+void Item::displayOrders() const
+{
+    std::cout << "Item " << this << " orders:\n" << mOrders;
+}
+
+ItemException::ItemException(const std::string& message)
+    : BaseException(message)
+{
+
+}
