d05c8977b7f6744874ff76f4503d1515f3c3d95c
Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>
2017-09-25 17:39:40 +0300

    c_tasks_pack_1: change directory for tasks
    Change-Id: Icd1c34bbdd11a86748b33547552ddf4cc0af932c
    Signed-off-by: Vladyslav Oliinyk <vladyslav.oliinyk@globallogic.com>

---
+++ c_tasks_pack_1/max2.c
@@ -0,0 +1,29 @@
+/*
+Задача max2
+
+Условие задачи
+Считать с клавиатуры два целых числа и вывести большее из них.
+Если числа равны, вывести любое.
+
+Пример ввода
+1 2
+Пример вывода
+2
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	int first;
+	int second;
+
+	scanf("%d %d", &first, &second);
+	if (first > second) {
+		printf("%d\n", first);
+	} else {
+		printf("%d\n", second);
+	}
+
+	return 0;
+}
